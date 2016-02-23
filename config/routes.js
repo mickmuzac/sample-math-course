c8c3ab808a2b33fcdc6dba9968e355f1fda85df4
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2016-02-23 17:11:06 -0500

    Initial xAPI submission working!

--- config/routes.js
+++ config/routes.js
@@ -6,20 +6,60 @@
 var mongoose = require('mongoose');
 var home = require('home');
 var question = require('question');
+var request = require('request');
 
 /**
  * Expose
  */
 
 function requireLogin(req, res, next){
-    if(!req.user) throw new Error("You must be logged in to continue");
+    if(req.query.xAPILaunchKey && req.query.xAPILaunchService){
+        getActorToken(req, res, next);
+    }
+    else if(!req.user && !req.session.xAPIActor){
+        throw new Error("You must be logged in to continue");
+    }
     else next();
+}
+
+function error(res, data){
+    res.status(500).send(data);
+}
+
+function getActorToken(req, res, next){
+    var path = "launch/" + req.query.xAPILaunchKey;
+    var service = req.query.xAPILaunchService;
+
+    var endpoint = service.slice(-1) === '/' ? service + path : service + '/' + path;
+
+    request.post(endpoint, (err, httpRes, body) => {
+        console.log("Cookies: ", httpRes.headers["set-cookie"]);
+
+        if(httpRes.headers["set-cookie"]) question.useCookie(httpRes.headers["set-cookie"][0]);
+
+        var data;
+        try{
+            var data = JSON.parse(body);
+        }
+        catch(e){
+            return error(res, body);
+        }
+
+        if(httpRes.statusCode === 200 && data){
+            req.session.xAPIActor = data;
+            res.redirect("/question/1");
+            console.log("Success!");
+        }
+        else{
+            return error(res, body);
+        }
+    });
 }
 
 module.exports = function (app, passport) {
 
   app.get('/', home.index);
-  app.use('/question', requireLogin, question);
+  app.use('/question', requireLogin, question.router);
 
   app.use('/login', passport.authenticate('github'));
   app.use('/logout', function(req, res){
